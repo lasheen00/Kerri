55b71576e93bddfc2af687d0807a6f7fb72a9f0b
lasheen00 <las7een00@gmail.com>
2025-03-19 05:05:58 +0200

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,17 @@
+
+  //!___________ Typed JS __________//!
+  var typed = new Typed('#element', {
+    strings: ["Kerri Deo","Designer","Devoloper"],
+    typeSpeed: 100,
+    backSpeed: 100,
+    loop: true
+  });
+
+  //!___________ Typed JS __________//!
+
+
+
+
+  //!___________AOS Animation__________//!
+  AOS.init();
+  //!___________AOS Animation__________//!
